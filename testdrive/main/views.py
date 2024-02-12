3d5c9c00803bea6c385f8535fc02153295d4c125
Tara Routra <s.gz@yandex.ru>
2024-02-12 23:44:57 +0300

    Вывод главной страницы в.1

--- testdrive/main/views.py
+++ testdrive/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Cars
 
-# Create your views here.
+
+def index(request):
+    cars = Cars.objects.order_by('id')
+    print(cars)
+    return render(request, 'main/index.html', {'title': 'Главная страница',
+                                               'cars': cars})
