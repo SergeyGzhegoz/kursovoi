40966a5996a733d8dddd233b026c13323cb31101
Tara Routra <s.gz@yandex.ru>
2024-02-14 23:45:57 +0300

    Редактирование шаблона Главной. Написание стилей Главной. Добавление записей в БД

--- testdrive/testdrive/settings.py
+++ testdrive/testdrive/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,6 +107,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
